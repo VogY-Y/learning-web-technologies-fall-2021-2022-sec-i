7c2e79c0f17a1b2462bba77d2f5f45c07d9bbc98
Md Hasibuzzaman Shanto <hshaan2@gmail.com>
2021-10-05 15:40:43 +0600

    updated email

--- MID_LAB_TASK_FORM_VALIDATION/Task_2/Email.php
+++ MID_LAB_TASK_FORM_VALIDATION/Task_2/Email.php
@@ -1,10 +1,20 @@
 <html>
 <body>
     <?php
-$Email = "";
+    $email = "";
+    $emailError ="";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $Email = test_input($_POST["email"]);
+
+if (empty($_POST["email"])) {
+    $emailError = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailError = "Invalid email format";
+    }
+  }
 }
+    
 function test_input($data) {
   $data = trim($data);
   $data = stripslashes($data);
@@ -12,16 +22,19 @@ function test_input($data) {
   return $data;
 }
 ?>
+    <p><span class="error">* required field</span></p>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         EMAIL<br><br>
         <input type="text" name="email">
+        <span class="error">* <?php echo $emailError;?></span>
         <br><br>
         <input type="submit" name="submit" value="Submit">
     </form>
-    <?php
-echo "<h1>Your email is</h1>";
-echo $Email;
 
+    <?php
+echo "<h1>Your Input:</h1>";
+echo $email;
 ?>
 </body>
+
 </html>
