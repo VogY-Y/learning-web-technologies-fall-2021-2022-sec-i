47de87164d700e42a699294be096209fb8e159e2
Md Hasibuzzaman Shanto <hshaan2@gmail.com>
2021-10-17 15:51:54 +0600

    mini project aded

---
+++ Mini_Project/userhomepage.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>User's Home Page</title>
+</head>
+<body>
+    <form>
+        <table>
+            <h1>Welcome Anne!</h1>
+            <tr>
+                <td>
+                    <a href="userProfile.php">Profile</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="Userchange_password.php">Change Password</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="logout.php">Logout</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
