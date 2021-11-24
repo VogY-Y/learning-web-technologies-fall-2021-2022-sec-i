eabbe9ec41efc81154313ff2afefca0b6af0c57d
Md Hasibuzzaman Shanto <hshaan2@gmail.com>
2021-11-24 20:31:09 +0600

    2nd commit

--- FINAL_LAB_PERFORMANCE_2/index.php
+++ FINAL_LAB_PERFORMANCE_2/index.php
@@ -5,6 +5,43 @@
 <center>
     <head>
         <title>Ajax Calculator</title>
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <style>
+            .but{
+                width: 25px;
+                height: 25px;
+                font-weight: bold;
+                text-align: center;
+            }
+            .cls{
+                width: 25px;
+                height: 25px;
+                font-weight: bold;
+                text-align: center;
+                color: #ffffff;
+                background-color: red;
+            }
+            .eql{
+                width: 100%;
+                height: 25px;
+                font-weight: bold;
+                text-align: center;
+                color: #ffffff;
+                background-color: green;
+            }
+            .display{
+                background-color: whitesmoke;
+                border:1px #666 solid; 
+                height: 50px; width: 150px; 
+                text-align: right; 
+                font-size: 20px;
+            }
+        </style>
+        <script>
+        	
+        	
+        </script>
+
         </head>
     <body>
 Simple Calculator Using AJAX/PHP
